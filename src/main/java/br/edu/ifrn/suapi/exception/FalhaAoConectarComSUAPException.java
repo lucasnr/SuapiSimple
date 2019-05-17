d0376b0ea8dd7b415f673856746662e22fdaaae5
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-05-17 15:25:20 -0300

    Documentação da exceção

--- src/main/java/br/edu/ifrn/suapi/exception/FalhaAoConectarComSUAPException.java
+++ src/main/java/br/edu/ifrn/suapi/exception/FalhaAoConectarComSUAPException.java
@@ -1,12 +1,27 @@
 package br.edu.ifrn.suapi.exception;
 
+import br.edu.ifrn.suapi.ClienteSUAP;
+
+/**
+ * Exceção para ser lançada durante a construção de um {@link ClienteSUAP} caso
+ * a conexão não possa ser estabelecida
+ * 
+ * @author Lucas do Nascimento Ribeiro
+ * @since 1.2
+ * @version 1.2
+ */
 public class FalhaAoConectarComSUAPException extends Exception {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Exceção pai desta exceção
+	 * 
+	 * @since 1.2
+	 */
 	private Exception parentException;
 
 	public FalhaAoConectarComSUAPException(Exception parentException) {
@@ -15,12 +30,12 @@ public class FalhaAoConectarComSUAPException extends Exception {
 
 	@Override
 	public String getMessage() {
-		return "Não foi possivel estabelecer uma conexão com o SUAP"; 
+		return "Não foi possivel estabelecer uma conexão com o SUAP";
 	}
-	
+
 	@Override
 	public synchronized Throwable getCause() {
 		return parentException.getCause();
 	}
-	
+
 }
